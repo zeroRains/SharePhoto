b06823d416fe4d0370276d2c5b1690f9e2465d67
zeroRains <2274033547@qq.com>
2021-08-22 10:49:05 +0800

    add the four fragments to the four items in the toolbar

--- android/app/src/main/java/com/example/sharephoto/MainActivity.java
+++ android/app/src/main/java/com/example/sharephoto/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sharephoto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,11 +16,13 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout tab_home, tab_channel, tab_message, tab_profile;
     private ImageView iv_home, iv_channel, iv_message, iv_profile;
     private ImageView current_position;
     private FloatingActionButton add_img;
+    private FragmentManager fragmentManager;
+    private Fragment home, channel, message, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initPager() {
+        fragmentManager = getSupportFragmentManager();
+
         add_img = findViewById(R.id.addImage);
         tab_home = findViewById(R.id.tab_home);
         tab_channel = findViewById(R.id.tab_channel);
@@ -44,36 +51,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tab_profile.setOnClickListener(this);
         tab_channel.setOnClickListener(this);
 
+        home = new Home();
+        channel = new Channel();
+        message = new Message();
+        profile = new Profile();
+
         current_position = iv_home;
         current_position.setSelected(true);
+        changePage(home);
+    }
+
+    private void changePage(Fragment f) {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.content, f);
+        ft.commit();
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         current_position.setSelected(false);
-        switch (id){
+        switch (id) {
             case R.id.tab_home:
                 iv_home.setSelected(true);
                 current_position = iv_home;
+                changePage(home);
                 break;
             case R.id.tab_channel:
                 iv_channel.setSelected(true);
                 current_position = iv_channel;
+                changePage(channel);
                 break;
             case R.id.tab_message:
                 iv_message.setSelected(true);
                 current_position = iv_message;
+                changePage(message);
                 break;
             case R.id.tab_profile:
                 iv_profile.setSelected(true);
                 current_position = iv_profile;
-                Intent intent = new Intent(MainActivity.this,Login.class);
-                startActivity(intent);
+                changePage(profile);
+//                Intent intent = new Intent(MainActivity.this, Login.class);
+//                startActivity(intent);
                 break;
             case R.id.addImage:
                 current_position.setSelected(true);
-                Toast.makeText(MainActivity.this,"click the add image",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "click the add image", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
