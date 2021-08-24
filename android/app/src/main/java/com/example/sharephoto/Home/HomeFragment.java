abab6eeb05a1922fbfa0cbc3b05720a3069b249e
PommesPeter <434596665@qq.com>
2021-08-24 20:32:03 +0800

    fixing confliction

--- android/app/src/main/java/com/example/sharephoto/Home/HomeFragment.java
+++ android/app/src/main/java/com/example/sharephoto/Home/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.example.sharephoto.Home;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.example.sharephoto.R;
 import com.google.android.material.tabs.TabLayout;
