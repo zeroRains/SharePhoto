a6878be9c2eac6fb449fc1d7737d6e23da427841
PommesPeter <434596665@qq.com>
2021-08-27 17:18:57 +0800

    updated splash activity

--- android/app/src/main/java/com/example/sharephoto/SplashActivity.java
+++ android/app/src/main/java/com/example/sharephoto/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.example.sharephoto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -17,29 +21,29 @@ public class SplashActivity extends AppCompatActivity {
 
     public OkHttpClient okHttpClient = new OkHttpClient();
     public OkHttpClient client = okHttpClient.newBuilder().build();
-    public String BaseUrl = "https://www.2020agc.site";
+    public String baseUrl = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        GetDemo();
+        getSplash();
     }
-    private void GetDemo() {
+    private void getSplash() {
         // get请求
         // 创建一个request对象
-        Request request = new Request.Builder().url(BaseUrl).build();
+        Request request = new Request.Builder().url(baseUrl).build();
         // 执行和回调
         client.newCall(request).enqueue(new Callback() {
-            public void onFailure(Call call, IOException e) {
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 // 请求失败
             }
-            public void onResponse(Call call, Response response)
+            public void onResponse(@NonNull Call call, @NonNull Response response)
                     throws IOException {
                 // 请求成功
                 String ret = response.body().string();
-                // 访问UI线程
-//                ToastUtil.ShortToast("连接服务器成功");
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(SplashActivity.this, "连接服务器成功啦 ヾ(≧▽≦*)o", Toast.LENGTH_SHORT).show());
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
