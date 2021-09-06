3bd04b6a6d99955a3fdb895c8e800921fa40edb1
PommesPeter <434596665@qq.com>
2021-09-06 17:38:48 +0800

    added upload avatar api

--- backEnd/api/imagesAPI.py
+++ backEnd/api/imagesAPI.py
@@ -1,9 +1,25 @@
-from . import server_info
-from model.image import Image
-from flask import Blueprint
+import json
+import os
+
+from flask import Blueprint, request
+
+from . import database_object
 
 image_opt = Blueprint("image_opt", __name__)
 
-@image_opt.route("")
-def query():
-    pass
+db = database_object
+
+
+@image_opt.route("/image/upload_avatar", methods=["POST"])
+def upload_avatar():
+    data = json.loads(request.get_data())
+    cursor = db.cursor()
+    try:
+        img = request.files.get('file')
+        path = "/static/img"
+        file_path = os.path.join(path, img.filename)
+        img.save(file_path)
+
+        return {"msg": "success", "data": [{"url": file_path}]}
+    except:
+        return {"msg": "failed", "data": []}
